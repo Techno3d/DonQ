565b79891ef9f02ac37c62b263a6287d96480b71
Shadman Syed <shadman3d@gmail.com>
2024-11-07 22:17:52 -0500

    Limit camera rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,7 +8,13 @@ public class CameraController : MonoBehaviour
     {
         float mousey = Input.GetAxis("Mouse Y"); 
         float rotY = -mousey*rotationSpeed*Mathf.Deg2Rad*Time.deltaTime*4;
-        rotY = Mathf.Clamp(rotY, -60, 80);
+        float funny = (transform.eulerAngles.x + 180)%360 - 180;
+        Debug.Log(funny + " " + rotY);
+        if(funny > 71 && rotY > 0) {
+            return;
+        } else if(funny < -50 && rotY < 0) {
+            return;
+        }
         transform.Rotate(new Vector3(rotY, 0f, 0f));
     }
 }
